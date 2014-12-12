7ff9ae90c7531a0ebfdf82ff68d98fbcc8be1853
uupaa <uupaa.js@gmail.com>
2014-12-12 10:05:37 +0900

    new cli

--- test/after.js
+++ test/after.js
@@ -1,44 +1,45 @@
-// @name: Foo.js
-
 (function(global) {
+"use strict";
 
-// --- variable --------------------------------------------
-// --- define ----------------------------------------------
-// --- interface -------------------------------------------
-function Foo(value) { // @arg Number: the value.
-                      // @help: Foo
+// --- dependency modules ----------------------------------
+// --- define / local variables ----------------------------
+//var _runOnNode = "process" in global;
+//var _runOnWorker = "WorkerLocation" in global;
+//var _runOnBrowser = "document" in global;
+
+// --- class / interfaces ----------------------------------
+function Foo(value) { // @arg Number - the value.
     this._value = value;
 }
 
-Foo["name"] = "Foo";
+//{@dev
 Foo["repository"] = "https://github.com/uupaa/Foo.js";
+//}@dev
+
 Foo["prototype"]["value"]     = Foo_value;     // Foo#value():Number
 Foo["prototype"]["isNumber"]  = Foo_isNumber;  // Foo#isNumber():Boolean
 Foo["prototype"]["isInteger"] = Foo_isInteger; // Foo#isInteger():Boolean
 
-// --- implement -------------------------------------------
-function Foo_value() { // @ret Number: get value.
+// --- implements ------------------------------------------
+function Foo_value() { // @ret Number - get value.
     return this._value;
 }
 
-function Foo_isNumber() { // @ret Boolean: valie is Number
+function Foo_isNumber() { // @ret Boolean - valie is Number
     return typeof this._value === "number";
 }
 
-function Foo_isInteger() { // @ret Boolean: valie is Integer
+function Foo_isInteger() { // @ret Boolean - valie is Integer
     return typeof this._value === "number" &&
            Math.ceil(this._value) === this._value;
 }
 
-// --- export ----------------------------------------------
-if (global["process"]) { // node.js
+// --- validate / assertions -------------------------------
+// --- exports ---------------------------------------------
+if ("process" in global) {
     module["exports"] = Foo;
 }
-if (global["Foo"]) {
-    global["Foo_"] = Foo; // already exsists
-} else {
-    global["Foo"]  = Foo;
-}
+global["Foo" in global ? "Foo_" : "Foo"] = Foo;
 
 })((this || 0).self || global);
 
